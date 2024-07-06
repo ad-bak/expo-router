0e3786d4a719518a4d1739b592efe113993f76bd
Adnan <adbak.dev@gmail.com>
2024-07-06 21:42:46 +0200

    fc

---
+++ src/app/about.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text } from "react-native";
+
+export default function About() {
+  return (
+    <>
+      <Text>about</Text>
+      <Link href="/">Go back</Link>
+    </>
+  );
+}
