eec273ccab8ef76a2fa5e67cecdcbd10282e12ad
Adnan <adbak.dev@gmail.com>
2024-07-07 22:30:14 +0200

    post list component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,12 @@
 import { Link } from "expo-router";
 import { Image, Text, View } from "react-native";
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
+
 import posts from "~/assets/data/posts.json";
+import PostListItem from "~/src/components/PostListItem";
 
 const post1 = posts[0];
 
 export default function FeedScreen() {
-  return (
-    <View>
-      <View>
-        <Image source={{ uri: post1.user.image_url }} className="w-24 aspect-square" />
-      </View>
-      <Image source={{ uri: post1.image_url }} className="w-full aspect-[4/3]" />
-    </View>
-  );
+  return <PostListItem post={post1} />;
 }
---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,32 @@
+import { View, Image, Text, useWindowDimensions } from "react-native";
+import { Ionicons, Feather, AntDesign } from "@expo/vector-icons";
+
+interface PostListItemProps {
+  post: {
+    user: {
+      image_url: string;
+      username: string;
+    };
+    image_url: string;
+  };
+}
+
+export default function PostListItem({ post }: PostListItemProps) {
+  return (
+    <View className="bg-white">
+      <View className="p-3 flex-row items-center gap-2">
+        <Image source={{ uri: post.user.image_url }} className="w-12 aspect-square rounded-full" />
+        <Text className="font-semibold">{post.user.username}</Text>
+      </View>
+      <Image source={{ uri: post.image_url }} className="w-full aspect-[4/3]" />
+
+      <View className="flex-row gap-3 p-3">
+        <AntDesign name="hearto" size={20} />
+        <Ionicons name="chatbubble-outline" size={20} />
+        <Feather name="send" size={20} />
+
+        <Feather name="bookmark" size={20} className="ml-auto" />
+      </View>
+    </View>
+  );
+}
